eae7bafccec49d1d4af04684b3b2d06e1fc36c9d
Bilal K <bilalk.@Bilals-MacBook-Air.local>
2022-08-15 16:46:53 +0500

    setting up the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-traversymedia';
+  title: string = 'task tracker'
 }
