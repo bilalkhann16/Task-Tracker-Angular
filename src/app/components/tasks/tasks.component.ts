ed192e259aa6cf5b2a4e7663c30f57e4e9d971e9
Bilal K <bilalk.@Bilals-MacBook-Air.local>
2022-08-16 14:39:07 +0500

    mock backend

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../../Task';
-import {TASKS} from '../../moct-tasks';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,10 +8,10 @@ import {TASKS} from '../../moct-tasks';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() { }
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>this.tasks = tasks);
   }
-
 }
